d477690dfb418c85782a9b47d6255372d3cf8604
Shajibul Alam Shihab <shihab@Shihab.Shajibul>
2023-05-17 10:35:19 +0600

    update

---
+++ src/Components/LeastSquares/LeastSquares.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LeastSquares = () => {
+  return <div>LeastSquares</div>;
+};
+
+export default LeastSquares;
---
+++ src/Components/SplineInterpolation/SplineInterpolation.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SplineInterpolation = () => {
+  return <div>SplineInterpolation</div>;
+};
+
+export default SplineInterpolation;
